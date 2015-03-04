f453fce4b311971cdf099b8161a92d917477fc49
Annelies Faes <annelies.faes@student.kuleuven.be>
2015-03-04 12:39:38 +0100

    POST ok voor Client (denk ik, kan het niet testen zonder server

--- src/HTTP.java
+++ src/HTTP.java
@@ -6,12 +6,14 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public abstract class HTTP {
 
 	protected String command;
 	protected String url;
+	protected String path;
 	protected int port;
 	protected Socket socket;
 	protected DataOutputStream outToServer;
@@ -19,11 +21,18 @@ public abstract class HTTP {
 	
 	public HTTP(String command, String url, int port){
 		this.command = command;
-		this.url = url;
 		this.port = port;
 		
+		this.url = url.split("/")[0];	
+		if(url.split("/").length < 2){
+			this.path = "";
+		}
+		else{
+			this.path = url.split("/")[1];
+		}
+		
 		try {
-			this.socket = new Socket(url, port);
+			this.socket = new Socket(this.url, this.port);
 			
 			this.outToServer = new DataOutputStream(socket.getOutputStream());
 	        this.inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));	        
@@ -39,15 +48,17 @@ public abstract class HTTP {
 	
 	public void sendRequest(){
         try{
-        	String sentence = this.command + " / " + "HTTP/1." + this.getHttpVersion(); //sentence is wat naar de server gestuurd moet worden
+        	String sentence = this.command + " /" + this.path + " HTTP/1." + this.getHttpVersion(); //sentence is wat naar de server gestuurd moet worden
         	outToServer.writeBytes(sentence + '\n' + '\n'); //TODO als tweede '\n' niet nodig is bij POST: 
-        													//zendt die apart in specifieke 'handlerequest'-methode, 
+        													//zend die apart in specifieke 'handlerequest'-methode, 
         													//of extra methode 'sendrequestspecific' die erbijgezet wordt in 
         													//de case bij 'handleresponse'-methode 
 			
         	handleResponse();
         	
-        	//close streams & socket?? (--> hangt af van antwoord assistent op vraag over of connection moet openblijven)
+        	this.outToServer.close();
+        	this.inFromServer.close();
+        	this.socket.close();
 		
 		} catch (IOException e) {
 		// TODO juist?
@@ -90,7 +101,17 @@ public abstract class HTTP {
 	
 	protected abstract void handlePutResponse();
 	
-	protected abstract void handlePostResponse();
+	protected void handlePostResponse() throws IOException{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Give a String for input, please:");
+		String input = scan.nextLine();
+		scan.close();
+		this.outToServer.writeBytes(input);
+		String line;
+		if((line = this.inFromServer.readLine()) != null){
+			System.out.println(line);
+		}	
+	}
 	
 	protected abstract int getHttpVersion();
 }
