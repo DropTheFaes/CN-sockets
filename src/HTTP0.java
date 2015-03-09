12e97f45f11a27e6fbd0714a03b5a51ae5e15fd3
Annelies Faes <annelies.faes@student.kuleuven.be>
2015-03-09 01:05:29 +0100

    imports :p

--- src/HTTP0.java
+++ src/HTTP0.java
@@ -1,20 +1,10 @@
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 
 public class HTTP0 extends HTTP{
